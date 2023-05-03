03da4d007773bdd9c2d01b531f430c83b0c73472
mahfuzurrahman4044 <mahfuzurrahman4044@gmail.com>
2023-05-03 18:40:03 +0600

    chef data

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = 3000
 
+const chef=require("./data/chefInfo.json")
+
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Food Village')
 })
+
+app.get('/chef', (req, res) => {
+  res.send(chef);
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
